75d535f56f636d69df8dbd9dac6f4e23edb1d63d
Alison Ferreira <alison.ferreira.82@gmail.com>
2020-10-24 14:25:43 -0300

    Initial commit based on Spring Boot Framework.

---
+++ src/main/java/br/com/transparencia/TransparenciaExporterApplication.java
@@ -0,0 +1,13 @@
+package br.com.transparencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransparenciaExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransparenciaExporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/transparencia/TransparenciaExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.transparencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransparenciaExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

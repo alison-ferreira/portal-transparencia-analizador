9a2d8a8afc86de389a2a40f508766cee649811ea
Alison Ferreira <alison.ferreira.82@gmail.com>
2020-10-26 23:52:07 -0300

    Concurrent and Reactive requests.

--- src/main/java/br/com/transparencia/event/listners/UnloadCardsTransactions.java
+++ src/main/java/br/com/transparencia/event/listners/UnloadCardsTransactions.java
@@ -1,5 +1,6 @@
 package br.com.transparencia.event.listners;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -12,6 +13,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import br.com.transparencia.jpa.entity.Transacao;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
 
 @Component
 public class UnloadCardsTransactions implements ApplicationListener<ApplicationReadyEvent> {
@@ -19,28 +22,32 @@ public class UnloadCardsTransactions implements ApplicationListener<ApplicationR
 	final static Logger logger = LoggerFactory.getLogger(UnloadCardsTransactions.class);
 
 	private WebClient client = WebClient.builder()
-			.baseUrl("http://www.portaltransparencia.gov.br")
-			.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-			.defaultHeader("chave-api-dados", "fb5454d37fd3d49d2a5859c3390b8f0d")
-			.build();
+				.baseUrl("http://www.portaltransparencia.gov.br")
+				.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+				.defaultHeader("chave-api-dados", "fb5454d37fd3d49d2a5859c3390b8f0d")
+				.build();
 
 	@Override
 	public void onApplicationEvent(ApplicationReadyEvent event) {
 		logger.info("Unload Cards Transactions has started the process.");
 		
 		logger.info("Retrieving from Government API.");
-		List<Transacao> transactionList = client.get()
-				.uri(uriBuilder -> uriBuilder.path("/api-de-dados/cartoes")
-						.queryParam("mesExtratoInicio", "01/2020")
-						.queryParam("mesExtratoFim", "01/2020")
-						.queryParam("pagina", "1")
-						.build())
-				.retrieve()
-				.bodyToFlux(Transacao.class)
-				.collectList()
-				.block();
-		
-		logger.info(transactionList.toString());
+		List<Integer> pages = Arrays.asList(1, 2, 3);
+		Flux.fromIterable(pages).parallel().runOn(Schedulers.elastic())
+			.flatMap(this::getTransactionsByPage)
+			.subscribe(t -> logger.info(t.toString()));
+//		this.getTransactionsByPage(1).subscribe(t -> logger.info(t.toString()));
+	}
+	
+	private Flux<Transacao> getTransactionsByPage(Integer page) {
+		return client.get()
+			.uri(uriBuilder -> uriBuilder.path("/api-de-dados/cartoes")
+					.queryParam("mesExtratoInicio", "01/2020")
+					.queryParam("mesExtratoFim", "01/2020")
+					.queryParam("pagina", page.toString())
+					.build())
+			.retrieve()
+			.bodyToFlux(Transacao.class);
 	}
 
 }
